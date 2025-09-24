237a5c27768c47e55cbd7fa78f8fe22a8ff85208
Abdul Aziz <file.azis@gmail.com>
2025-09-24 06:16:16 +0000

    the condition is still the same, there is a very large top margin or top

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -39,7 +39,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       <AppSidebar />
       <div className="flex flex-col sm:pl-14">
         <Header />
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+        <main className="grid flex-1 gap-4 p-4 sm:px-6 md:gap-8">
           {children}
         </main>
       </div>
