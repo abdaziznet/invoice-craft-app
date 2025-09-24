7f544756226119421b9b183944589808dd7731de
Abdul Aziz <file.azis@gmail.com>
2025-09-24 07:28:58 +0000

    tidy up the navbar, give it some space because it's too close to the top

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -34,11 +34,13 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <div className="flex min-h-screen w-full flex-col">
-      <Header />
-      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        {children}
-      </main>
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <Header />
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+          {children}
+        </main>
+      </div>
     </div>
   );
 }
