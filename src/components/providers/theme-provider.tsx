2a8268a95ee51e18c3cb240195894be06cfb207f
Abdul Aziz <file.azis@gmail.com>
2025-09-24 05:00:29 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'ne

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -7,8 +7,3 @@ import { type ThemeProviderProps } from "next-themes/dist/types"
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
-
-// Add next-themes to package.json dependencies
-// "next-themes": "^0.3.0"
-// I cannot modify package.json, so I've copied the required code from the package.
-// This is not ideal, but it is the only way to meet the requirements.
